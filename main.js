918adfccafc284f93b96ce312980e05967562a57
0mar-cc <147678239+0mar-cc@users.noreply.github.com.obal user.email>
2023-12-30 01:55:42 +0200

    Finished from grids and mouse events to start and stop hovering

--- main.js
+++ main.js
@@ -1,44 +1,151 @@
 // Create link to add css file
-let link = document.createElement("link")
+const link = document.createElement("link")
 link.rel = "stylesheet";
 link.href = "style.css";
 document.head.append(link)
 
 // Create a div to contain the sketchbox
-let container = document.createElement("div");
+const container = document.createElement("div");
 container.className = "container";
 
 // Create the ScetchBox
-let sketchBox = document.createElement("div");
+const sketchBox = document.createElement("div");
 sketchBox.className = "sketchBox";
 
 
+let width = sketchBox.style.width = "480px";
+let height = sketchBox.style.height = "480px";
 
+width = Number(width.replace("px",""));
+height = Number(height.replace("px",""));
 
+let boxWidth = (width - 80) ;
+let boxHeight = (height - 80) ;
 
-function createGrids(){
+let sixteenDim = (((boxWidth) /16) );
+console.log(sixteenDim)
+
+let mouse = false;
+
+function createGrid(){
     
-    let earse = document.createElement("button");
-    earse.className = "earse";
-    earse.textContent = "Earse";
-
-    for (i= 1; i < 257 ; i++){
-        let grid = document.createElement("div"); 
-        grid.className = "grid";
-        sketchBox.append(grid)
-    
-        grid.addEventListener('mouseenter',function(){grid.style.backgroundColor='gold';})
+    const reset = document.createElement("button");
+        reset.className = "reset";
+        reset.textContent = "Reset";
         
-        earse.addEventListener("click",function(){
-            grid.style.backgroundColor="white";
-        })
-    }
-    container.append(earse)
-} 
+    for (let i = 0 ; i < 256 ; i++){
+        
+        const grid = document.createElement("div");
+        grid.className = "grid";
+        grid.style.width = `${sixteenDim}px`;
+        grid.style.height = `${sixteenDim}px`;
+        sketchBox.append(grid)
+        
 
-createGrids()
+        reset.addEventListener("click",function(){
+            grid.style.backgroundColor="white";
+            mouse = true;
+        })
+
+        if(mouse == false){
+            sketchBox.addEventListener('mousedown',function(){grid.onmouseenter=function(){grid.style.backgroundColor='gold'}})
+            sketchBox.addEventListener('mouseup',function(){grid.onmouseenter=function(){return true}})
+        }
+
+        else{
+            mouse = true;
+        } 
+    }
+    container.append(reset)
+}
+createGrid()
 
 
 // Adding the divs to html file
 container.append(sketchBox);
 document.body.append(container);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let width = sketchBox.style.width = "480px";
+// let height = sketchBox.style.height = "480px";
+
+// width = Number(width.replace("px",""));
+// height = Number(height.replace("px",""));
+
+// let boxWidth = width;
+// let boxHeight = height;
+
+// let sixteenDim = ((boxWidth /16) - 2 );
+// console.log(sixteenDim)
+
+
+// function createGrids(){
+    
+//     let earse = document.createElement("button");
+//     earse.className = "earse";
+//     earse.textContent = "Earse";
+
+    
+//         let grid = document.createElement("div"); 
+//         grid.className = "grid";
+//         grid.style.width = `${sixteenDim}px`;
+//         grid.style.height = `${sixteenDim}px`;
+//         sketchBox.append(grid)
+        
+//         let mouse = false;
+//         function mousedown()
+//         {
+//         mouse = true;
+//         callEvent();
+//         }
+//         function mouseup()
+//         {
+//         mouse =false;
+//         }
+//         function callEvent()
+//         {
+//         if(mouse)
+//         {
+//             grid.style.backgroundColor = "gold";        
+//             // it will continue executing until mouse is not released
+
+//         setTimeout("callEvent()",1);
+//         }
+//         else
+//         return;
+//         }
+//         console.log(i)
+//     }
+//     container.append(earse)
+
+
+// createGrids()
+
+
